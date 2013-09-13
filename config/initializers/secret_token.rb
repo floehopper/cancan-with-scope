1c2828c490e7e4711969af6b4373971138ea9ef8
James Mead <james@floehopper.org>
2013-09-13 18:02:46 +0100

    Initial commit - vanilla Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CancanWithScope::Application.config.secret_key_base = '2fd3027ba68b444563d493423b3595a0e65bc2be89bb56c8216c9dadb46d1ad19aaa781c102f4ab2eeee15a32dd2aa896deb32c36f5a54589f8747100ee38381'
